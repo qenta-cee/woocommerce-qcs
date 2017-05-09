a71168c86ed4395da17b94092b92d7119e2c2bc3
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2017-05-09 09:44:22 +0200

    Add MO language files

--- woocommerce-wirecard-checkout-seamless/templates/back.php
+++ woocommerce-wirecard-checkout-seamless/templates/back.php
@@ -1,22 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 </head>
 <body>
-<h3><?php __( 'You will be redirected shortly' , 'woocommerce-wirecard-checkout-seamless' ); ?></h3>
-<p><?php __( 'If not, please click ', 'woocommerce-wirecard-checkout-seamless' ); ?><a href="#" onclick="iframeBreakout()"><?php __( 'here', 'woocommerce-wirecard-checkout-seamless' ); ?></a></p>
+<h3><?php echo __( 'You will be redirected shortly', 'woocommerce-wirecard-checkout-seamless' ); ?></h3>
+<p><?php echo __( 'If not, please click ', 'woocommerce-wirecard-checkout-seamless' ); ?>
+	<a href="#" onclick="iframeBreakout()"><?php echo __( 'here', 'woocommerce-wirecard-checkout-seamless' ); ?></a>
+</p>
 <form method="POST" name="redirectForm" action="<?php echo $url; ?>" target="_parent">
-	<input type="hidden" name="redirected" value="1" />
+	<input type="hidden" name="redirected" value="1"/>
 	<?php
-	foreach ($_REQUEST as $k => $v)
-	{
-		printf('<input type="hidden" name="%s" value="%s" />', htmlspecialchars($k), htmlspecialchars($v));
+	foreach ( $_REQUEST as $k => $v ) {
+		printf( '<input type="hidden" name="%s" value="%s" />', htmlspecialchars( $k ), htmlspecialchars( $v ) );
 	}
 	?>
 </form>
 <script type="text/javascript">
-	function iframeBreakout()
-	{
+	function iframeBreakout() {
 		document.redirectForm.submit();
 	}
 
