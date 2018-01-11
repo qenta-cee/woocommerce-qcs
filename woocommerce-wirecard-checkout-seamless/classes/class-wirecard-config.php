8ce082ca9d244bf8b2a41d42e09144a46d8c7691
sdcwdcee <srdjan.dzombic@wirecard.com>
2018-01-11 12:10:54 +0100

    #51 Version number increase

--- woocommerce-wirecard-checkout-seamless/classes/class-wirecard-config.php
+++ woocommerce-wirecard-checkout-seamless/classes/class-wirecard-config.php
@@ -31,7 +31,7 @@
  */
 
 define( 'WOOCOMMERCE_GATEWAY_WCS_NAME', 'WirecardCheckoutSeamless' );
-define( 'WOOCOMMERCE_GATEWAY_WCS_VERSION', '1.0.8' );
+define( 'WOOCOMMERCE_GATEWAY_WCS_VERSION', '1.0.10' );
 
 /**
  * Config class
