c5352eaf096bc753f7a2cc2c18e22370ff85e90a
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-09-07 14:20:58 +0200

    Fix syntax error in init.sh, use sed for wait abort

--- scripts/wait-for-shop.sh
+++ scripts/wait-for-shop.sh
@@ -3,18 +3,15 @@
 # entrypoint of shop now puts 'ready' in a file after installation of
 # wordpress, woocommerce and plugin
 
-function read_log() {
-  docker exec -it woocommerce cat /tmp/shop.log
-  #docker exec -it woocommerce "tail -f /path/to/file.log | sed '/^ready/ q'"
-}
+docker exec -it woocommerce tail -f /tmp/shop.log | sed '/^ready/ q'
+
+# function read_log() {
+#   docker exec -it woocommerce cat /tmp/shop.log
+#   #docker exec -it woocommerce "tail -f /path/to/file.log | sed '/^ready/ q'"
+# }
 
 # LOG_CONTENT=$(read_log)
 # echo "Waiting for Shop Setup to finish"
 # while [[ -z $(read_log | grep ready) ]]; do
 #   sleep 1;
 # done
-
-echo "Waiting for webserver"
-while ! curl --fail -k https://localhost:443; do 
-  sleep 10
-done
