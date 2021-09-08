421b8262486099b7b45f782f791351ac34e91074
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-09-08 08:59:24 +0200

    Wait for log entry ready

--- scripts/wait-for-shop.sh
+++ scripts/wait-for-shop.sh
@@ -2,18 +2,18 @@
 
 # entrypoint of shop now puts 'ready' in a file after installation of
 # wordpress, woocommerce and plugin
-docker exec woocommerce touch /tmp/shop.log
-docker exec woocommerce cat /tmp/shop.log
-docker exec woocommerce cat /tmp/debug.log
-timeout 15m docker exec woocommerce tail -f /tmp/shop.log | sed '/^ready/ q'
-docker exec woocommerce cat /tmp/shop.log
-# function read_log() {
-#   docker exec -it woocommerce cat /tmp/shop.log
-#   #docker exec -it woocommerce "tail -f /path/to/file.log | sed '/^ready/ q'"
-# }
+# docker exec woocommerce touch /tmp/shop.log
+# docker exec woocommerce cat /tmp/shop.log
+# docker exec woocommerce cat /tmp/debug.log
+# timeout 15m docker exec woocommerce tail -f /tmp/shop.log | sed '/^ready/ q'
 
-# LOG_CONTENT=$(read_log)
-# echo "Waiting for Shop Setup to finish"
-# while [[ -z $(read_log | grep ready) ]]; do
-#   sleep 1;
-# done
+function read_log() {
+  docker exec woocommerce cat /tmp/shop.log
+  #docker exec -it woocommerce "tail -f /path/to/file.log | sed '/^ready/ q'"
+}
+
+LOG_CONTENT=$(read_log)
+echo "Waiting for Shop Setup to finish"
+while [[ -z $(read_log | grep ready) ]]; do
+  sleep 1;
+done
